16e7000d3eb19bb94078bd1bdbf8f5afa00ed35c
hieule210924tb <manu210924@gmail.com>
2026-04-22 15:48:13 +0700

    Migration Laravel

---
+++ database/migrations/2026_04_22_073157_create_comment_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            // Để khớp với int(10) trong ảnh của bạn
+            $table->increments('id');
+
+            $table->string('name', 200)->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id(); // ID của bảng comment (mặc định BigInt cũng được)
+            $table->text('content')->nullable();
+
+            // Khai báo cột post_id kiểu Integer (4 bytes) để khớp với increments('id') ở trên
+            $table->unsignedInteger('post_id');
+
+            // Thiết lập khóa ngoại
+            $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
+            //postId : là khóa ngoại, on('post'): liên kết với bảng post, cascadeOnDelete(): xóa id là xóa cmt
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment');
+        Schema::dropIfExists('posts');
+    }
+};
