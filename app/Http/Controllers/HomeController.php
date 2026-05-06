32aba61f8c487f51143e9a7447d464eb8d7a3d23
hieule210924tb <manu210924@gmail.com>
2026-05-06 16:58:07 +0700

    ORM (Eloquent) trong Laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -10,10 +11,42 @@ class HomeController
 {
     public function index()
     {
-        //QUERY Builder 
-        $rel = DB::table('users')->get(); // Lấy ra tất cả các dữ liệu
-        $rel2 = DB::table('users')->first(); // lấy ra 1 dòng dữ liệu đầu tiên
-        $rel3 = DB::table('users')->orderBy('created_at', 'desc')->get(); // Lấy ra tất cả các dữ liệu và sắp xếp giảm dần theo created_at
+        //Cách 3: ORM
+        $rel = Post::all(); // lấy ra toàn bộ dữ liệu
+        $rel2 = Post::find(2); // lấy ra 1 dòng dữ liệu có id =2
+        $rel3 = Post::orderBy('created_at', 'desc')->get()->toArray(); //Lấy theo điều kiện
+        //limit, offset
+        $rel10 = Post::offset(1)->limit(2)->get()->toArray();
+        $rel11 = Post::skip(1)->take(2)->get()->toArray();
+        dd($rel11);
+        //cách 1
+        //insert+
+        // $post = new Post;
+        // $post->name = 'Bài test';
+        // $post->content = 'Nội dung bài test';
+        // $post->save();
+        //update+
+        // $post = Post::find(3); // tìm id =3
+        // $post->name = 'Đã sửa 3';
+        // $post->save();
+        //delete+
+        // $post = Post::find(3);
+        // $post->delete();
+        //cách 2
+        //insert+
+        // $rel4 = Post::create([
+        //     'name' => 'Test cách 2',
+        //     'content' => 'Nội dung test 2'
+        // ]);
+        //update+
+        // $rel5 = Post::where('id', 4)->update(['name' => 'updated id là 4']);
+        //delete+
+        // Post::destroy('id', 4);
+        // echo $rel5;
+        //Cách 2: QUERY Builder 
+        // $rel = DB::table('users')->get(); // Lấy ra tất cả các dữ liệu
+        // $rel2 = DB::table('users')->first(); // lấy ra 1 dòng dữ liệu đầu tiên
+        // $rel3 = DB::table('users')->orderBy('created_at', 'desc')->get(); // Lấy ra tất cả các dữ liệu và sắp xếp giảm dần theo created_at
         // $rel4 = DB::table('users')->insert([
         //     'name' => 'Le Van Hieu 23',
         //     'email' => 'hle232e9@gmail.com',
@@ -24,10 +57,10 @@ class HomeController
         //     'name' => 'Nguyễn Văn B',
         //     'email' => 'ngvanb@gmail.com'
         // ]); // update dữ liệu
-        $rel6 = DB::table('users')->where('id', 13)->delete(); // Xóa dữ liệu
-        echo ($rel6);
+        // $rel6 = DB::table('users')->where('id', 13)->delete(); // Xóa dữ liệu
+        // echo ($rel6);
 
-        // SQL thuần
+        // Cách 1: SQL thuần
         // $rel = DB::select("SELECT * FROM users");
         // $dated = date('Y-m-d H:i:s');
         // $rel = DB::insert("INSERT INTO users (name, email,password,created_at) VALUES ('hieua3', 'haie6@gmail.com','123456','$dated')");
