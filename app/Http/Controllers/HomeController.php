41dd98e70957e49b4283366805f8465d775664b8
hieule210924tb <manu210924@gmail.com>
2026-05-06 14:40:21 +0700

    Tìm hiểu về Query Builder Laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController
 {
     public function index()
     {
+        //QUERY Builder 
+        $rel = DB::table('users')->get(); // Lấy ra tất cả các dữ liệu
+        $rel2 = DB::table('users')->first(); // lấy ra 1 dòng dữ liệu đầu tiên
+        $rel3 = DB::table('users')->orderBy('created_at', 'desc')->get(); // Lấy ra tất cả các dữ liệu và sắp xếp giảm dần theo created_at
+        // $rel4 = DB::table('users')->insert([
+        //     'name' => 'Le Van Hieu 23',
+        //     'email' => 'hle232e9@gmail.com',
+        //     'password' => Hash::make('123456'),
+        //     'created_at' => date("Y-m-d H:i:s")
+        // ]); // thêm dữ liệu
+        // $rel5 = DB::table('users')->where('id', 2)->update([
+        //     'name' => 'Nguyễn Văn B',
+        //     'email' => 'ngvanb@gmail.com'
+        // ]); // update dữ liệu
+        $rel6 = DB::table('users')->where('id', 13)->delete(); // Xóa dữ liệu
+        echo ($rel6);
+
+        // SQL thuần
         // $rel = DB::select("SELECT * FROM users");
         // $dated = date('Y-m-d H:i:s');
         // $rel = DB::insert("INSERT INTO users (name, email,password,created_at) VALUES ('hieua3', 'haie6@gmail.com','123456','$dated')");
         // $rel = DB::update("UPDATE users set name ='hiele235' where users.id =4");
-        $rel = DB::delete("DELETE from users where users.id = 8");
-        echo $rel;
+        // $rel = DB::delete("DELETE from users where users.id = 8");
+        // echo $rel;
         //$email = $request->input('email');
 
         //$request->input : lấy dữ liệu của các ô input trong form
