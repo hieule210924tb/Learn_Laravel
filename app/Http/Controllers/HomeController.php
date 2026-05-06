50e084b3aa5744184a10c95c70c402838e4cf026
hieule210924tb <manu210924@gmail.com>
2026-05-06 13:47:35 +0700

    Làm việc với Database Laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController
 {
-    public function index(Request $request)
+    public function index()
     {
-
+        // $rel = DB::select("SELECT * FROM users");
+        // $dated = date('Y-m-d H:i:s');
+        // $rel = DB::insert("INSERT INTO users (name, email,password,created_at) VALUES ('hieua3', 'haie6@gmail.com','123456','$dated')");
+        // $rel = DB::update("UPDATE users set name ='hiele235' where users.id =4");
+        $rel = DB::delete("DELETE from users where users.id = 8");
+        echo $rel;
         //$email = $request->input('email');
 
         //$request->input : lấy dữ liệu của các ô input trong form
@@ -21,11 +27,11 @@ class HomeController
         //     return 'Đã upload thành công vào thư mục' . $path;
         // }
         //$request->validate : kiểm tra validate
-        $validated = $request->validate([
-            'fullname' => 'required | min:5 | max: 40',
-            'email' => 'required |email'
-        ]);
-        return 'Dữ liệu hợp lệ' . $validated['fullname'];
+        // $validated = $request->validate([
+        //     'fullname' => 'required | min:5 | max: 40',
+        //     'email' => 'required |email'
+        // ]);
+        // return 'Dữ liệu hợp lệ' . $validated['fullname'];
     }
     public function index2(Request $request)
     {
